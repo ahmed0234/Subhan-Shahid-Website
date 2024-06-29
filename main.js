644a8220be7becfc334ff7908c073dd1ae319d6a
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-06-29 15:50:46 +0500

    What Services i Do

--- main.js
+++ main.js
@@ -1,50 +1,7 @@
 import "/main.css";
-import LocomotiveScroll from "locomotive-scroll";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-
-function locomotiveBoilerPlate() {
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
-  });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  locoScroll.on("scroll", ScrollTrigger.update);
-
-  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform
-      ? "transform"
-      : "fixed",
-  });
-
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
-
-  // START FROM HERE ABOVE IS BOILERPLATE
-}
-
-locomotiveBoilerPlate();
 
 let tl = gsap.timeline();
 
@@ -72,31 +29,31 @@ let tl = gsap.timeline();
 
 // ===============
 
-tl.to("#introdiv", {
-  delay: 3.5,
-  duration: 0.5,
-  ease: `sine.out`,
-  opacity: 0,
-  display: "none",
-});
-tl.from(".reveal", {
-  opacity: 0,
-  y: 100,
-  duration: 0.6,
-  ease: "sine.out",
-  stagger: 0.1,
-});
+// tl.to("#introdiv", {
+//   delay: 3.5,
+//   duration: 0.5,
+//   ease: `sine.out`,
+//   opacity: 0,
+//   display: "none",
+// });
+// tl.from(".reveal", {
+//   opacity: 0,
+//   y: 100,
+//   duration: 0.6,
+//   ease: "sine.out",
+//   stagger: 0.1,
+// });
 
-tl.from(".img_reveal", {
-  top: `130%`,
-  duration: 0.5,
-  ease: `sine.inOut`,
-  delay: `-0.95`,
-});
-tl.from(".nav_reveal", {
-  y: -80,
-  opacity: 0,
-  duration: 0.6,
-  ease: "power3.out",
-  delay: `-0.1`,
-});
+// tl.from(".img_reveal", {
+//   top: `130%`,
+//   duration: 0.5,
+//   ease: `sine.inOut`,
+//   delay: `-0.95`,
+// });
+// tl.from(".nav_reveal", {
+//   y: -80,
+//   opacity: 0,
+//   duration: 0.6,
+//   ease: "power3.out",
+//   delay: `-0.1`,
+// });
